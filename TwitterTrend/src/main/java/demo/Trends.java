061333eb52c602e6a5e8f285de399c633c55265a
swethabobba03@gmail.com <chanu@Macintosh-3.local>
2015-05-12 19:09:27 -0700

    Twitter Trend

---
+++ TwitterTrend/src/main/java/demo/Trends.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Trends {
+
+	private TwitterTrend[] trends;
+
+	public TwitterTrend[] getTrends() {
+		return trends;
+	}
+
+	public void setTrends(TwitterTrend[] trends) {
+		this.trends = trends;
+	}
+		
+}
