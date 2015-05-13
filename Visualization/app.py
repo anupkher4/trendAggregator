90980661f5a98a484e67b404c45b7489c782ffc0
Jawahar Santosh Gundapaneni <jawaharsantosh@gmail.com>
2015-05-13 10:12:54 -0700

    For FourSquare Visualization. REST call to fetch foursquare data

--- Visualization/app.py
+++ Visualization/app.py
@@ -15,15 +15,7 @@ FIELDS = {'school_state': True, 'resource_type': True, 'poverty_level': True, 'd
 
 @app.route("/")
 def index():
-    return render_template("fs.html")
-
-@app.route("/instagram")
-def index1():
-    return render_template("instagram.html")
-
-@app.route("/twitter")
-def index2():
-    return render_template("twitter.html")
+    return render_template("index.html")
 
 @app.route("/trendaggregator/foursquare")
 def trendaggregator_foursquare():
@@ -32,36 +24,6 @@ def trendaggregator_foursquare():
 	#db.authenticate('root','test123')
     collection = connection[DBS_NAME]['fourSquareTrend']
     projects = collection.find({},{'name': 1,'category': 1, 'checkinsCounts': 1, 'usersCount': 1, 'state': 1, 'insertedTime': 1, '_id': 0})
-    json_projects = []
-    for project in projects:
-        json_projects.append(project)
-    json_projects = json.dumps(json_projects, default=json_util.default)
-    connection.close()
-	#MongoClient.close()
-    return json_projects
-
-@app.route("/trendaggregator/instagram")
-def instagram():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    connection[DBS_NAME].authenticate('root','test123')
-	#db.authenticate('root','test123')
-    collection = connection[DBS_NAME]['instagramTrend']
-    projects = collection.find({},{'caption': 1,'commentCount': 1, 'likesCount': 1, 'created_time': 1, 'url': 1, '_id': 1, 'latitude': 1, 'longitude': 1})
-    json_projects = []
-    for project in projects:
-        json_projects.append(project)
-    json_projects = json.dumps(json_projects, default=json_util.default)
-    connection.close()
-	#MongoClient.close()
-    return json_projects
-
-@app.route("/trendaggregator/twitter")
-def twitter():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    connection[DBS_NAME].authenticate('root','test123')
-	#db.authenticate('root','test123')
-    collection = connection[DBS_NAME]['twitterTrend']
-    projects = collection.find({},{'name': 1,'created_at': 1, 'as_of': 1, 'url': 1, '_id': 0})
     json_projects = []
     for project in projects:
         json_projects.append(project)
